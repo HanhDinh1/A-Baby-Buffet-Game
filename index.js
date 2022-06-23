68f5aaf710d52b82abcff77a7ee52d9dc5e7d6bf
HanhDinh <ms.myhanh89@gmail.com>
2022-06-23 12:06:55 -0400

    colussion detected works, spoon works, foods work

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ function startGame(){
     const ctx = canvas.getContext('2d')
     let loadedImageObjects =null;
     let myBaby;
+    let spoon;
     let donut;
     let coffee;
     let broccoli;
@@ -56,12 +57,23 @@ function startGame(){
         this.vX =0;
         this.vY = 0;
         this.width = width;
-        this.height=height;
+        this.height = height;
     }
 
     updatePosition (){
         this.x += this.vX
         this.y += this.vY
+    }
+
+    crashWith (target){
+        if (this.x < target.x + target.width &&
+            this.x + this.width > target.x &&
+            this.y < target.y + target.height &&
+            this.height + this.y > target.y) {
+         return true;
+        } else {
+           return false;
+        }
     }
 
     // draw(){
@@ -73,9 +85,13 @@ class ImageObject extends RectangleObject {
     constructor(x, y, width, height, imageElement){
        super(x, y, width, height);
        this.image = imageElement;
+       this.sx = 0;
+       this.sy = 0;
+       this.swidth = width;
+       this.sheight = height;
     }
     draw(){
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.sx, this.sy, this.swidth, this.sheight, this.x, this.y, this.width, this.height);
     }
 }
 
@@ -116,6 +132,12 @@ class ImageObject extends RectangleObject {
     loadImages(sources, function(images){
        loadedImageObjects = images;
        myBaby = new ImageObject(canvas.width/2, canvas.height -190, 80, 175, images.baby);
+       myBaby.swidth = 891;
+       myBaby.sy = 2112;
+       myBaby.sheight = 2112;
+
+       //spoon = new ImageObject(0, 0, 50, 30, images.spoon);
+
        //donut = new ImageObject(0, 0, 30, 30, images.donut)
        //coffee = new ImageObject(0, 30, 30, 30, images.coffee)
        //broccoli = new ImageObject(0, 60, 30, 30, images.broccoli)
@@ -143,11 +165,11 @@ class ImageObject extends RectangleObject {
                 let foodY = undefined;
 
                 if (Math.random() > 0.5){
-                    foodX = Math.floor(Math.random() * (canvas.width * 0.18))
+                    foodX = Math.floor(Math.random() * (canvas.width * 0.17))
                     foodY = Math.floor(Math.random() * canvas.height)      
                 }
                 else{
-                    foodX = Math.floor(Math.random() * canvas.width * 0.18 + (canvas.width * 0.82))
+                    foodX = Math.floor(Math.random() * canvas.width * 0.17 + (canvas.width * 0.81))
                     foodY = Math.floor(Math.random() * canvas.height)  
                 }
 
@@ -155,7 +177,10 @@ class ImageObject extends RectangleObject {
             }
             // babyPosition.updatePosition();
             myBaby.updatePosition();
-
+            if(spoon != undefined){
+                spoon.updatePosition();
+            }
+           
             //drawings
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             ctx.drawImage(loadedImageObjects.restaurant, 0, 0, canvas.width, canvas.height);
@@ -164,12 +189,19 @@ class ImageObject extends RectangleObject {
             };
         
             myBaby.draw();
-           
+
+            if (spoon != undefined){
+                spoon.draw()
+            }
+            for (let i =0; i< foodArray.length; i++){
+                if(spoon != undefined){
+                    if (spoon.crashWith(foodArray[i])){
+                        foodArray.splice(i, 1)
+                        spoon = undefined
+                    }                   
+                }            
+            };
         }
-
-    
-
-        
 
 //PROJECTILE
 // class Projectile{
@@ -186,18 +218,50 @@ class ImageObject extends RectangleObject {
 //         ctx.fillStyle = this.color
 //         ctx.fill()
 //             }
+//     update(){
+//         this.draw()
+//         this.x = this.x + this.velocity.x
+//         this.y = this.y + this.velocity.y
+//     }
 //     }
 
+//     let babyPosition =  myBaby.updatePosition();
+//     const baby = new Baby (x, y, )
+
+// //projectile array
+//     const projectile = new Projectile(
+//         // event.clientX,
+//         // event.clientY,
+//         canvas.width/2,
+//         canvas.height/2,
+//         5, 
+//         'pink',
+//         {
+//             x = 1,
+//             y = 1
+//         }
+//     )
+//     const projectiles = [projectile]
+
+//     function animate(){
+//         requestAnimationFrame(animate)
+//         projectiles.forEach ((projectile) =>{
+//             projectile.update()
+//         })
+
+//         // projectile.draw()
+//         // projectile.update()
+//     }
 
 // addEventListener('click', (event)=>{
-// // console.log('go')
-// const Projectile = new projectile(
-//     event.foodX,
-//     event.foodY,
-
-
-// )
+// // console.log(event)
+//const angle = Math.atan2(event.clientY - canvas.height/2, 
+//event.clientX - canvas.width/2)
+// projectiles.push(new Projectile(
+//     canvas.width/2, canvas.height/2, 5, 'pink', {x:1, y: 1}
+// ))
 // })
+// animate()
 //END PROJECTILE
     
 
@@ -205,16 +269,37 @@ class ImageObject extends RectangleObject {
                 switch(event.code){
                     case 'ArrowLeft':
                         myBaby.vX -=1;
+                        myBaby.sy = 0;
                         break;
                     case 'ArrowRight':
                         myBaby.vX +=1;
+                        myBaby.sy = 2112;
                         break;
                     case 'ArrowUp':
+                        event.preventDefault();
                         myBaby.vY -=1;
                         break;
                     case 'ArrowDown':
+                        event.preventDefault();
                         myBaby.vY +=1;
                         break;   
+                    case 'Space':
+                        event.preventDefault();
+                        spoon = new ImageObject(myBaby.x, myBaby.y, 32, 32, loadedImageObjects.spoon)
+                        if (myBaby.sy === 2112){
+                            spoon.sx = 0;
+                            spoon.sy = 16;
+                            spoon.swidth = 16;
+                            spoon.sheight = 16;
+                            spoon.vX = 2;
+                        }
+                        else{
+                        spoon.sx = 0;
+                        spoon.sy = 0;
+                        spoon.swidth = 16;
+                        spoon.sheight = 16;
+                        spoon.vX = -2;
+                        }
                 }
             })
 
