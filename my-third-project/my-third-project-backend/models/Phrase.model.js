7057849435fa8ad20c390b8bd4572a3ab656d313
Hanh Dinh <103163170+HanhDinh1@users.noreply.github.com>
2022-12-04 00:02:47 -0500

    Delete my-third-project/my-third-project-backend directory

--- my-third-project/my-third-project-backend/models/Phrase.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const {Schema, model } = mongoose;
-
-const phraseSchema = new Schema({
-    engPhrase: String,
-    selectedLang: String
-});
-
-module.exports = model('Phrase', phraseSchema);
